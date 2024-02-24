2dd9c77de8c830289a2369c4e2e12755564336a5
Michael Schmitz <michael.schmitz@swissuccess.ch>
2024-02-24 14:00:44 +0100

    Implement a PriorityQueue based on a circular Array

--- DuA/LinearDS/src/main/java/ch/authenticit/study/Main.java
+++ DuA/LinearDS/src/main/java/ch/authenticit/study/Main.java
@@ -1,31 +1,40 @@
 package ch.authenticit.study;
 
+import ch.authenticit.study.queue.ArrayPriorityQueue;
 import ch.authenticit.study.queue.ArrayQueue;
 import ch.authenticit.study.queue.StackQueue;
 
 public class Main {
     public static void main(String[] args) {
-        var arrQueue = new StackQueue();
-        arrQueue.enqueue(10);
-        arrQueue.enqueue(20);
-        arrQueue.enqueue(30);
-        System.out.println(arrQueue.dequeue());
-        System.out.println(arrQueue.peek());
-
-        arrQueue.enqueue(40);
-        arrQueue.enqueue(50);
-        System.out.println(arrQueue.isFull());
-        arrQueue.enqueue(60);
-        System.out.println(arrQueue.isFull());
-        System.out.println(arrQueue.dequeue());
-        System.out.println(arrQueue.dequeue());
-        System.out.println(arrQueue.dequeue());
-        arrQueue.enqueue(70);
-        System.out.println(arrQueue.dequeue());
-        System.out.println(arrQueue.dequeue());
-        System.out.println(arrQueue.dequeue());
-        System.out.println(arrQueue.isEmpty());
-        arrQueue.enqueue(80);
-        System.out.println(arrQueue.isEmpty());
+        var apq = new ArrayPriorityQueue(5);
+        System.out.println(apq.isEmpty());
+        apq.enqueue(30);
+        apq.enqueue(20);
+        apq.enqueue(10);
+        apq.enqueue(50);
+        System.out.println(apq.dequeue());
+        apq.enqueue(40);
+        System.out.println(apq.dequeue());
+        System.out.println(apq.isEmpty());
+        System.out.println(apq.isFull());
+        apq.enqueue(10);
+        apq.enqueue(5);
+        System.out.println(apq);
+        apq.dequeue();
+        apq.dequeue();
+        apq.enqueue(20);
+        apq.enqueue(30);
+        System.out.println(apq);
+        apq.dequeue();
+        apq.dequeue();
+        apq.dequeue();
+        apq.dequeue();
+        apq.enqueue(15);
+        apq.enqueue(-5);
+        System.out.println(apq);
+        apq.enqueue(20);
+        apq.enqueue(99);
+        System.out.println(apq.isFull());
+        System.out.println(apq);
     }
 }
---
+++ DuA/LinearDS/src/main/java/ch/authenticit/study/queue/ArrayPriorityQueue.java
@@ -0,0 +1,96 @@
+package ch.authenticit.study.queue;
+
+import java.util.Arrays;
+
+public class ArrayPriorityQueue implements IQueue {
+    private final int[] items;
+    private int front = 0;
+    private int rear = 0;
+    private int count = 0;
+    private final int size;
+
+    public ArrayPriorityQueue(int size) {
+        this.size = size;
+        items = new int[size];
+    }
+
+    @Override
+    public void enqueue(int item) {
+        if (isFull())
+            throw new IllegalStateException("Cannot enqueue on a full Queue");
+
+        if (isEmpty()) {
+            items[rear++] = item;
+            rear = rear % size;
+            count++;
+            return;
+        }
+
+        var current = Math.floorMod((rear - 1), size);
+
+        while (current != rear) {
+            // shift items and move current
+            if (items[current] > item) {
+                items[(current + 1) % size] = items[current];
+                current = Math.floorMod((current - 1), size);
+                continue;
+            }
+            // insert item
+            items[(current + 1) % size] = item;
+            rear = (rear + 1) % size;
+            count++;
+            return;
+        }
+        // insert at the front, Queue is now full
+        items[front] = item;
+        rear = (rear + 1) % size;
+        count++;
+    }
+
+    @Override
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException("Cannot dequeue on an empty Queue");
+
+        var val = items[front];
+        items[front] = 0;
+        front++;
+        front = front % size;
+        count--;
+
+        return val;
+    }
+
+    @Override
+    public int peek() {
+        return items[front];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return count == items.length;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty())
+            return "[]";
+
+        var result = new int[count];
+
+        var starter = front;
+        var i = 0;
+        do {
+            result[i] = items[starter++];
+            starter = starter % items.length;
+            i++;
+        } while (starter != rear);
+
+        return Arrays.toString(result);
+    }
+}
