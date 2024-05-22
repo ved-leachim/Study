38839b12ca4d3a661ff34b3d2bb887af41f873cb
Michael Schmitz <michael.schmitz@swissuccess.ch>
2024-05-22 22:24:15 +0200

    Learn Tailwind

---
+++ Tailwind CSS/tailwind-course-starter/plugins/buttonPlugin.js
@@ -0,0 +1,32 @@
+const plugin = require("tailwindcss/plugin");
+const { default: lightOrDarkColor } = require("@check-light-or-dark/color");
+
+const buttonPlugin = plugin(function({ addComponents, matchComponents, theme }) {
+  console.log(theme("spacing")["0.5"]);
+  addComponents({
+    ".btn": {
+      display: "inline-block",
+      cursor: "pointer",
+      fontWeight: "bold",
+      padding: `${theme("spacing.2")} ${theme("spacing.4")}`,
+      borderRadius: theme("borderRadius").lg
+    }
+  });
+
+  console.log(theme("colors"));
+  for (let key in theme("colors")) {
+    if (typeof theme("colors")[key] !== "string") {
+      for (let shade in theme("colors")[key]) {
+        const colorType = lightOrDarkColor(theme("colors")[key][shade]);
+        addComponents({
+          [`.btn-${key}-${shade}`]: {
+            backgroundColor: theme("colors")[key][shade],
+            color: colorType === "dark" ? "white" : "black"
+          }
+        });
+      }
+    }
+  }
+});
+
+export default buttonPlugin;
